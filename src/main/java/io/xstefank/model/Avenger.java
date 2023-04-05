d4294d2f8d9dff0d437f45e4fead04e4dac0b0e2
xstefank <xstefank122@gmail.com>
2023-04-05 13:52:30 +0200

    Add database with Panache and REST data

---
+++ src/main/java/io/xstefank/model/Avenger.java
@@ -0,0 +1,14 @@
+package io.xstefank.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Avenger extends PanacheEntity {
+
+    public String name;
+
+    public String civilName;
+    public boolean snapped;
+
+}

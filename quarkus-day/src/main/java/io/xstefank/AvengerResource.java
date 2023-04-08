0b0e260a72c551c4bacd9e4a74688cf0515c8b2d
xstefank <xstefank122@gmail.com>
2023-04-08 14:43:02 +0200

    Replace with avenger-service

--- quarkus-day/src/main/java/io/xstefank/AvengerResource.java
+++
@@ -1,7 +0,0 @@
-package io.xstefank;
-
-import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
-import io.xstefank.model.Avenger;
-
-public interface AvengerResource extends PanacheEntityResource<Avenger, Long> {
-}

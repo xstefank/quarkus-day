ad5566d67843beb2a48fa897eeb6c9d678135905
xstefank <xstefank122@gmail.com>
2023-04-08 17:41:48 +0200

    Add avenger-service dev mode testing

--- avenger-service/src/main/java/io/xstefank/GreetingResource.java
+++ avenger-service/src/main/java/io/xstefank/GreetingResource.java
@@ -1,16 +1,39 @@
 package io.xstefank;
 
+import io.quarkus.logging.Log;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Path("/api")
 public class GreetingResource {
 
+    private AtomicInteger counter = new AtomicInteger(0);
+
     @GET
+    @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello from Brno Java Meetup";
     }
+
+    @GET
+    @Path("/hello2")
+    public String hello2() {
+        return "Hello 2";
+    }
+
+
+
+    @GET
+    @Path("/counter")
+    public void counter() {
+        Log.info("Something ");
+        Log.info("Counting change " + counter.getAndIncrement());
+    }
+
+
 }
--- avenger-service/src/test/java/io/xstefank/GreetingResourceTest.java
+++ avenger-service/src/test/java/io/xstefank/GreetingResourceTest.java
@@ -12,10 +12,35 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/api/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .body(is("Hello from Brno Java Meetup"));
     }
+
+    @Test
+    public void testHelloEndpoint2() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        given()
+            .when().get("/api/hello")
+            .then()
+            .statusCode(200)
+            .body(is("Hello from Brno Java Meetup"));
+    }
+
+
+    @Test
+    public void testHello2Endpoint() {
+        given()
+            .when().get("/api/hello2")
+            .then()
+            .statusCode(200)
+            .body(is("Hello 2"));
+    }
+
 
 }
